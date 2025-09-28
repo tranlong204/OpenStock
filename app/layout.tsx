031d4d723829bb0b6fe7ca6cdb7cfb51cb87c5be
Mr. Algorithm <11aravipratapsingh@gmail.com>
2025-09-29 00:53:31 +0530

    created the absic routing and setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en" className="dark">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
