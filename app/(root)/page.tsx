295a8a13635de2fe11bcb2a2b44925f013d082e3
Mr. Algorithm <11aravipratapsingh@gmail.com>
2025-09-30 10:13:08 +0530

    Implemented home page with TradingView widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,52 @@
 import React from 'react'
-import {Button} from "../../components/ui/button";
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG} from "@/lib/constants";
 
 const Home = () => {
+    const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
     return (
         <div className="flex min-h-screen home-wrapper">
-            <div className="flex flex-col items-center justify-center w-full">
-                <h1 className="text-4xl font-bold mb-4">Welcome to OpenStock</h1>
-                <p className="text-lg mb-8 text-center max-w-2xl">
-                    OpenStock is an open-source alternative to expensive market platforms.
-                    Track real-time prices, set personalized alerts, and explore detailed company insights.
-                </p>
-                <Button>Get Started</Button>
-            </div>
+            <section className="grid w-full gap-8 home-section">
+                <div className="md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        title="Market Overview"
+                        scriptUrl={`${scriptUrl}market-overview.js`}
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+
+                </div>
+                <div className="md:col-span-1 xl:col-span-2">
+                    <TradingViewWidget
+                        title="Stock Heatmap"
+                        scriptUrl={`${scriptUrl}stock-heatmap.js`}
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+                </div>
+            </section>
+            <section className="grid w-full gap-8 home-section">
+                <div className="md:col-span-1 xl:col-span-2">
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}market-quotes.js`}
+                        config={MARKET_DATA_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+                <div className="md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}timeline.js`}
+                        config={TOP_STORIES_WIDGET_CONFIG}
+                        height={600}
+                    />
+
+                </div>
+
+            </section>
         </div>
+
     )
 }
 export default Home
