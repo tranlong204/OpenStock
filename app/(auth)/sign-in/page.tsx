fff6efe0e84767a3b9cc64e69aed32497a3336f7
Mr. Algorithm <11aravipratapsingh@gmail.com>
2025-10-04 22:29:25 +0530

    updated defauts

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -51,7 +51,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@jsmastery.com"
+                    placeholder="opendevsociety@cc.cc"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
